bd18e62b04d532a6ee16684f4b1c193bd1f250fa
begosrs <begosrunescape@gmail.com>
2021-07-28 19:50:54 +0100

    Bug fixes

--- js/map.js
+++ js/map.js
@@ -68,12 +68,14 @@ $(document).ready(function () {
         }
         map.addControl(L.control.zoom());
         map.addControl(new PlaneControl());
-        map.addControl(new LocationLookupControl());
-        map.addControl(new MapLabelControl());
-        map.addControl(new CollectionControl({position: 'topright'}));
-        map.addControl(new RegionLookupControl());
-        map.addControl(new GridControl());
-        map.addControl(new RegionLabelsControl());
+        if (!script) {
+            map.addControl(new LocationLookupControl());
+            map.addControl(new MapLabelControl());
+            map.addControl(new CollectionControl({position: 'topright'}));
+            map.addControl(new RegionLookupControl());
+            map.addControl(new GridControl());
+            map.addControl(new RegionLabelsControl());
+        }
     }
 
     if (!mode) {
